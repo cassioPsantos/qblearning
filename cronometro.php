0c6b111be69a4ffd64aa8129a493bd6f9f9369f7
RedesIFC <cassio12456@gmail.com>
2022-09-23 10:15:03 -0300

    Cronometro

--- cronometro.php
+++ cronometro.php
@@ -7,6 +7,29 @@ include('navbar.php');
 </head>
 
 <body>
+    <span id="tempo">20</span><br>
+    <button onclick="para()" >PARA</button>
+    <button onclick="inicia()" >INICIA</button>
+    <button onclick="salva()" >SALVA</button>
+
     ESSA PAGINA É UM CRONOMETRO
+    <script>
+        let tempo = 0;
+        let timer;
+
+        function inicia(){
+            tempo=0
+            timer = setInterval(() => {
+                tempo += 0.01;
+                document.getElementById('tempo').innerHTML = tempo.toFixed(2);
+            }, 10);
+        }
+        function para(){
+            clearInterval(timer);
+        }
+        function salva(){
+            window.location.href = '/qblearning/salvacronometro.php?tempo='+tempo.toFixed(2)
+        }
+    </script>
 </body>
 </html>
---
+++ salvacronometro.php
@@ -0,0 +1,8 @@
+<?php include('config.php');
+include('conexao.php');
+$tempo = $_GET['tempo'];
+var_dump($tempo)
+// $sql = "INSERT INTO tempo (sla, sla sla, tempo) 
+//             VALUES (sla, sla, sla, '$tempo')";
+//     mysqli_query($conn, $sql);
+?>
